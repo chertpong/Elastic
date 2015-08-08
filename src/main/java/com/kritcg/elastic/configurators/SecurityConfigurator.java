9a8eb12a056b88fe7dedf0c5f0d15221d16b978c
Chertpong <chertpong_h@cmu.ac.th>
2015-08-08 21:19:31 +0700

    ~disable csrf

--- src/main/java/com/kritcg/elastic/configurators/SecurityConfigurator.java
+++ src/main/java/com/kritcg/elastic/configurators/SecurityConfigurator.java
@@ -15,6 +15,7 @@ public class SecurityConfigurator extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf().disable()
             .httpBasic().and()
             .authorizeRequests()
             .antMatchers("/", "/search/all","/search/id/**","/search/keywords/**").permitAll()
